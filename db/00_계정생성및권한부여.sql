10097b8a5fa08bd7517e85818efb1db49bd43a58
ockko <ocko119@gmail.com>
2025-05-13 15:32:50 +0900

    [실습]계정생성 및 권한부여

---
+++ db/00_계정생성및권한부여.sql
@@ -0,0 +1,23 @@
+-- 계정만들기
+create user '계정명'@'%' identified by '비밀번호';
+create user '계정명'@'localhost' identified by '비밀번호';
+
+-- mysql 데이터베이스 사용
+use mysql;
+
+-- 계정 조회하기
+select user, user.host from user;
+
+-- 데이터베으스(스키마) 생성
+create database 데이터베이스이름;
+create schema 데이터베이스이름;
+
+create database testdb;
+
+-- 권한부여
+-- grant 권한 privileges on 데이터베이스.테이블 to '계정명'@'%';
+-- ockko 계정이 testdb에 대한 모든 권한을 부여
+grant all privileges on testdb.* to 'ockko'@'%';
+
+-- ockko@%가 가지고 있는 모든 권한 조회
+show grants for 'ockko'@'%';
