c4073666eecab00e780c9f0640246c8722b0b58b
ockko <ocko119@gmail.com>
2025-05-21 17:53:34 +0900

    [실습] JDBC

---
+++ db/07_jdbc/jdbc_lecture/src/test/java/junit/AssertionsTest.java
@@ -0,0 +1,27 @@
+package junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AssertionsTest {
+    @Test
+    void assertTests() {
+        // 두 값이 동일한지 확인
+//        assertEquals(4, 2 * 3, "곱셈 결과 확인");
+
+        // 조건식이 true
+//        assertTrue(5 == 1, "조건이 참인지 확인");
+
+//        Object obj = new Object();
+//        assertNotNull(obj, "객체가 null이 아님을 확인");
+
+        // 특정 예외가 발생하는지 확인
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            int result = 1 / 0;
+        });
+
+        // 예외 메시지 확인
+        assertEquals("/ by zero", exception.getMessage());
+    }
+}
